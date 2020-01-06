7627914caca5344cae83434ecaf13d71ddafc60b
hoanggiang521999 <hoanggiang521999@gmail.com>
2020-01-06 23:48:01 +0700

    #master

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 // server.js
 // where your node app starts
+
 // init project
 const express = require("express");
 const app = express();
-const PORT = 8008
+
+// we've started you off with Express,
+// but feel free to use whatever libs or frameworks you'd like through `package.json`.
+
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static("public"));
 
@@ -11,9 +15,10 @@ app.use(express.static("public"));
 app.get("/", function(request, response) {
   response.sendFile(__dirname + "/view/index.html");
 });
+
 // listen for requests :)
-const listener = app.listen(PORT, function() {
-  console.log("Your app is listening on port: " + PORT);
+const listener = app.listen(process.env.PORT, function() {
+  console.log("Your app is listening on port " + listener.address().port);
 });
 
 module.exports = app;
